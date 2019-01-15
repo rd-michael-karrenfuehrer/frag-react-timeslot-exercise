71a998b93a3e33ce2cc0a282920c5d2d3414d979
matthias.loeks@rewe-digital.com <matthias.loeks@rewe-digital.com>
2019-01-15 13:37:22 +0100

    NOTICKET Add enzyme for tests
    Also adjust boilerplate test to use Enzyme and test something.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+it('renders application header', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.App-header').exists()).toBe(true);
 });
